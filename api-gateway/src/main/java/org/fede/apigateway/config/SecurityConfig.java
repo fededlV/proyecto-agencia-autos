fa543121f157fbba187a49973fc07c134adf3ac0
fededlV <fededelavega2@gmail.com>
2024-11-11 21:02:33 -0300

    api y seguridad andando

--- api-gateway/src/main/java/org/fede/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/fede/apigateway/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
@@ -45,7 +46,25 @@ public class SecurityConfig  {
 
     //Metodo para convertir los roles de JWT a GrantedAuthority
     private JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        var grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+        //Especificar el nombre del claim a analizar
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("realm_access.roles");
+        // Agregar prefijo por convencion de Spring
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+        // Se asocia el conversor de Authorities al Bean que convierte el token JWT a un objeto Authorization
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation("https://labsys.frc.utn.edu.ar/aim/realms/backend-tps");
+    }
+}
+
+/*JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
             Collection<GrantedAuthority> authorities = new ArrayList<>();
             Map<String, Object> realmAcces = (Map<String, Object>) jwt.getClaims().get("realm_access");
@@ -60,11 +79,4 @@ public class SecurityConfig  {
             }
             return authorities;
          });
-        return jwtAuthenticationConverter;
-    }
-
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        return JwtDecoders.fromIssuerLocation("https://labsys.frc.utn.edu.ar/aim/realms/backend-tps");
-    }
-}
+        return jwtAuthenticationConverter;*/
